7a0838b7670cd73909ad8352c22aa001f952740e
Bojidar Dermendjiew <bojidardermendjiew@abv.bg>
2023-11-17 20:09:11 +0200

    uplaod JSON Processing

---
+++ 08. Exercise JSON Processing/ProductShop/ProductShop/ProductShopProfile.cs
@@ -0,0 +1,14 @@
+﻿namespace ProductShop
+{
+    using AutoMapper;
+
+    using Models;
+    using DTOs.Import;
+    public class ProductShopProfile : Profile
+    {
+        public ProductShopProfile()
+        {
+            this.CreateMap<ImportUserDto, User>();
+        }
+    }
+}
---
+++ 08. Exercise JSON Processing/ProductShop/ProductShop/StartUp.cs
@@ -0,0 +1,36 @@
+﻿namespace ProductShop
+{
+    using AutoMapper;
+    using Newtonsoft.Json;
+
+    using Data;
+    using Models;
+    using DTOs.Import;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            ProductShopProfile context = new ProductShopProfile();
+            string output = File.ReadAllText(@"../../../");
+        }
+
+        public static string ImportUsers(ProductShopContext context, string inputJson)
+        {
+            IMapper mapper = new Mapper(new MapperConfiguration(cfg =>
+             {
+                 cfg.AddProfile<ProductShopProfile>();
+             }));
+            var userDtos = JsonConvert.DeserializeObject<ImportUserDto[]>(inputJson);
+            ICollection<User> validUser = new HashSet<User>();
+            foreach (var userDto in userDtos)
+            {
+                User user = mapper.Map<User>(userDto);
+                validUser.Add(user);
+            }
+            context.Users.AddRange(validUser);
+            context.SaveChanges();
+            return $"Successfully imported {validUser.Count}";
+        }
+    }
+}
